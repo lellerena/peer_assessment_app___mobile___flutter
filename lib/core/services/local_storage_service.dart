c095751184ecca072724a98892dfd13837d3a0d4
Jhon Jimenez <zuletajj@uninorte.edu.co>
2025-09-10 04:12:06 -0500

    Add Local storage service

---
+++ lib/core/services/local_storage_service.dart
@@ -0,0 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorageService {
+  Future<void> saveCredentials(String email, String password) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('email', email);
+    await prefs.setString('password', password);
+  }
+
+  Future<Map<String, String>?> getCredentials() async {
+    final prefs = await SharedPreferences.getInstance();
+    final email = prefs.getString('email');
+    final password = prefs.getString('password');
+    if (email != null && password != null) {
+      return {'email': email, 'password': password};
+    }
+    return null;
+  }
+
+  Future<void> clearCredentials() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('email');
+    await prefs.remove('password');
+  }
+}
