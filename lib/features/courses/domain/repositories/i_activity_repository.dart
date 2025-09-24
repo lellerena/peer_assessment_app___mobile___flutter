a93b4ed03d58f1454b103d9573748491e2530e59
lellerena <llerenaluiseduardo3@gmail.com>
2025-09-24 07:43:27 -0500

    Add IActivityRepository and ActivityUseCase implementations

---
+++ lib/features/courses/domain/repositories/i_activity_repository.dart
@@ -0,0 +1,10 @@
+import '../models/activity.dart';
+
+abstract class IActivityRepository {
+  Future<List<Activity>> getActivitiesByCourseId(String courseId);
+  Future<bool> addActivity(Activity activity);
+  Future<bool> updateActivity(Activity activity);
+  Future<bool> deleteActivity(String activityId);
+  Future<Activity?> getActivityById(String activityId);
+  Future<List<Activity>> getActivitiesByCategoryId(String categoryId);
+}
